8c09ad5046c472ddec466c65e0de6a73a5c39bc0
matzr75 <matz@MacBook-Pro.local>
2017-03-13 21:49:16 +0100

    Test

--- ReadFile.py
+++ ReadFile.py
@@ -43,11 +43,26 @@ def openDBconnection():
     else:
         return cnx
     
+def getLastDate():
+# Get next entry from the queue
+
+  con = openDBconnection()
+  cursor = con.cursor()
+  cursor.execute("SELECT max(DateTime) from T_PowerLog")
+
+  record = cursor.fetchone()
+
+  con.close()
+
+  return record[1]
+
 # ============
 #     Start
 # ============
 
-startDate = sys.argv[1]
+#startDate = sys.argv[1]
+startDate = getLastDate()
+print (startDate)
 
 url = "http://10.0.1.58/solar_api/v1/GetArchiveData.cgi?Scope=System&StartDate=" + startDate + "&EndDate=" + startDate + "&Channel=EnergyReal_WAC_Sum_Produced&Channel=TimeSpanInSec&Channel=EnergyReal_WAC_Plus_Absolute&Channel=EnergyReal_WAC_Minus_Absolute"
 
@@ -57,24 +72,24 @@ StartDate = datetime.datetime.strptime(data["Body"]["Data"]["meter:16501544"]["S
 dataArray = ConvertJSON(data)
 
 data_dict = ConvertJSON(data)
+print(data_dict)
+# con = openDBconnection()
+# cursor = con.cursor()
+# row_count = 0
 
-con = openDBconnection()
-cursor = con.cursor()
-row_count = 0
-
-for row in data_dict:
+# for row in data_dict:
     
-    secOffset = row[0] + 3600
-    timestamp = StartDate + datetime.timedelta(seconds=secOffset)
+#     secOffset = row[0] + 3600
+#     timestamp = StartDate + datetime.timedelta(seconds=secOffset)
     
-    sql = "INSERT INTO T_PowerLog (DateTime, EnergyReal_WAC_Sum_Produced, EnergyReal_WAC_Plus_Absolute, EnergyReal_WAC_Minus_Absolute) VALUES (%s, %s, %s, %s)"
-    values = (timestamp, row[3], row[1], row[2]) 
-    cursor.execute(sql, values)
-    recordID = cursor.lastrowid
-    row_count += 1
+#     sql = "INSERT INTO T_PowerLog (DateTime, EnergyReal_WAC_Sum_Produced, EnergyReal_WAC_Plus_Absolute, EnergyReal_WAC_Minus_Absolute) VALUES (%s, %s, %s, %s)"
+#     values = (timestamp, row[3], row[1], row[2]) 
+#     cursor.execute(sql, values)
+#     recordID = cursor.lastrowid
+#     row_count += 1
     
-con.commit()
-cursor.close()
-con.close()
+# con.commit()
+# cursor.close()
+# con.close()
     
-print(str(row_count) + " records written")
+#print(str(row_count) + " records written")
